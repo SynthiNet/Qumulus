bf0654c46e70e9eb879d7d65eecccb0563034276
Frank Erens <frank@synthi.net>
2013-04-01 17:17:29 +0200

    Added vimproject stuff.

---
+++ .vimproject/tpl/tpl.cpp
@@ -0,0 +1,9 @@
+/*
+ * Qumulus UML editor
+ * COPYRIGHT HERE
+ *
+ */
+
+#include "HEADER_HERE"
+
+// Code here
---
+++ .vimproject/tpl/tpl.h
@@ -0,0 +1,12 @@
+/*
+ * Qumulus UML editor
+ * COPYRIGHT HERE
+ *
+ */
+
+#ifndef HEADER_GUARD_HERE
+#define HEADER_GUARD_HERE
+
+
+// Code here
+
