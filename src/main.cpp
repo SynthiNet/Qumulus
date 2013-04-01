2420934ac4be8f0760c54bea986459576142ebf2
Frank Erens <frank@synthi.net>
2013-04-01 17:18:43 +0200

    main.cpp skeleton files.

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * Qumulus UML editor
+ *
+ * Author: Frank Erens
+ */
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QWidget>
+
+int main(int argc, char** argv) {
+    QApplication app(argc, argv);
+
+    QWidget w;
+    w.show();
+
+    return app.exec();
+}
---
+++ test/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Qumulus UML editor
+ *
+ * Author: Frank Erens
+ */
+
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
