18eeba9cf5a21a50dd810ed5dc87e6af07289f64
Randy Thiemann <uselinuxnow@gmail.com>
2013-04-15 10:02:38 +0200

    Add more icons.

---
+++ src/Qumulus/Gui/Core/All.h
@@ -0,0 +1,15 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#ifndef QUMULUS_GUI_CORE_ALL_H_
+#define QUMULUS_GUI_CORE_ALL_H_
+
+#include "internal_base.h"
+
+#include "Nyi.h"
+
+#endif /* QUMULUS_GUI_CORE_ALL_H_ */
+
