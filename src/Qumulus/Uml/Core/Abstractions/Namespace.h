4304641179ac02649e551d9d630fd25901b3fe87
Frank Erens <frank@synthi.net>
2013-04-15 14:06:20 +0200

    Namespace stuff.

--- src/Qumulus/Uml/Core/Abstractions/Namespace.h
+++ src/Qumulus/Uml/Core/Abstractions/Namespace.h
@@ -11,13 +11,26 @@
 
 #include "NamedElement.h"
 
+#include <unordered_map>
+
 QUML_BEGIN_NAMESPACE_UCA
 
 class Namespace : public NamedElement {
 public:
+    Namespace() = default;
+    Namespace(const Namespace& n);
+
+    const std::unordered_map<QString, uptr<NamedElement>>& 
+            ownedMembers() const {
+        return mOwnedMembers;
+    }
+
+    void addMember(uptr<NamedElement> e);
+
 
     QUML_CLONABLE(Namespace)
 private:
+    std::unordered_map<QString, uptr<NamedElement>> mOwnedMembers;
 };
 
 QUML_END_NAMESPACE_UCA
