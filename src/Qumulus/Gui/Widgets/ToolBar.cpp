3048d4be974eaa35f318c228956e4ea6e7d568fa
Randy Thiemann <uselinuxnow@gmail.com>
2013-04-15 12:32:23 +0200

    Add ToolBar.

---
+++ src/Qumulus/Gui/Widgets/ToolBar.cpp
@@ -0,0 +1,46 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#include "ToolBar.h"
+#include <Gui/Widgets/MainWindow.h>
+
+QUML_BEGIN_NAMESPACE_GW
+
+#ifdef Q_OS_MAC
+ToolBar::ToolBar() : mToolBar(new QMacUnifiedToolBar()) {}
+#else
+ToolBar::ToolBar() : mToolBar(new QToolBar()) {}
+#endif
+
+void ToolBar::showInWindow(MainWindow* w) {
+    mWindow = w;
+#ifdef Q_OS_MAC
+    mToolBar->showInWindowForWidget(w);
+#else
+    w->addToolBar(mToolBar);
+#endif
+}
+
+QuGW::MainWindow* ToolBar::window() {
+    return mWindow;
+}
+
+QAction* ToolBar::addAction(const QIcon& icon, const QString& text) {
+    return mToolBar->addAction(icon, text);
+}
+
+void ToolBar::addSeparator() {
+    mToolBar->addSeparator();
+}
+
+#ifdef Q_OS_MAC
+void ToolBar::addFlexibleSpace() {
+    mToolBar->addStandardItem(QMacToolButton::FlexibleSpace);
+}
+#endif
+
+QUML_END_NAMESPACE_GW
+
---
+++ src/Qumulus/Gui/Widgets/ToolBar.h
@@ -0,0 +1,49 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#ifndef GUI_WIDGETS_TOOLBAR_H_
+#define GUI_WIDGETS_TOOLBAR_H_
+
+#include "internal_base.h"
+#include <QtCore/QObject>
+#include <QtCore/QString>
+#include <QtWidgets/QAction>
+#include <QtGui/QIcon>
+#ifdef Q_OS_MAC
+#include <QtMacExtras/QMacUnifiedToolBar>
+#else
+#include <QtWidgets/QToolBar>
+#endif
+#include <Lib/Core/Ptr.h>
+
+QUML_BEGIN_NAMESPACE_GW
+
+class MainWindow;
+
+class ToolBar : public QObject {
+public:
+    ToolBar();
+    void showInWindow(MainWindow* w);
+    MainWindow* window();
+    QAction* addAction(const QIcon& icon, const QString& text);
+    void addSeparator();
+#ifdef Q_OS_MAC
+    void addFlexibleSpace();
+#endif
+
+private:
+#ifdef Q_OS_MAC
+    uptr<QMacUnifiedToolBar> mToolBar;
+#else
+    uptr<QToolBar> mToolBar;
+#endif
+    MainWindow* mWindow;
+};
+
+QUML_END_NAMESPACE_GW
+
+#endif /* GUI_WIDGETS_TOOLBAR_H_ */
+
