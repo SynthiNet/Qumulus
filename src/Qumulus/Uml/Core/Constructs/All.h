2b0ed46248366ce365991307bc0c1071ea300688
Frank Erens <frank@synthi.net>
2013-04-15 11:08:56 +0200

    Add internal_base.h,All.h for Constructs and Diagram

---
+++ src/Qumulus/Uml/Core/Constructs/All.h
@@ -0,0 +1,12 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_CORE_CONSTRUCTS_ALL_H_
+#define UML_CORE_CONSTRUCTS_ALL_H_
+
+
+#endif /* UML_CORE_CONSTRUCTS_ALL_H_ */
+
---
+++ src/Qumulus/Uml/Core/Constructs/internal_base.h
@@ -0,0 +1,21 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_CORE_CONSTRUCTS_INTERNAL_BASE_H_
+#define UML_CORE_CONSTRUCTS_INTERNAL_BASE_H_
+
+#define QUML_BEGIN_NAMESPACE_UCA \
+    namespace Qumulus { namespace Uml { namespace Core { \
+        namespace Constructs { 
+#define QUML_END_NAMESPACE_UCA }}}}
+
+QUML_BEGIN_NAMESPACE_UCA
+QUML_END_NAMESPACE_UCA
+
+namespace QuUCA = Qumulus::Uml::Core::Constructs;
+
+#endif /* UML_CORE_CONSTRUCTS_INTERNAL_BASE_H_ */
+
