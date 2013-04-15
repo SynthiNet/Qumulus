8f16e96ef6f69d3703450c16e3c32a42c8eb7950
Frank Erens <frank@synthi.net>
2013-04-15 14:06:44 +0200

    LiteralSpecification testcase.

---
+++ test/Uml/Core/Abstractions/LiteralSpecification.cpp
@@ -0,0 +1,18 @@
+/*
+ * Qumulus UML Editor 
+ * Author: Frank Erens
+ *
+ */
+
+#include <gtest/gtest.h>
+#include <Uml/Core/Abstractions/LiteralSpecification.h>
+
+namespace {
+
+    class TestLiteralSpecification : public ::testing::Test {
+
+    };
+
+} // namespace
+
+
