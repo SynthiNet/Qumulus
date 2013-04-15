9a60b03ff765c05024a7d747110323feb4d1fd26
Frank Erens <frank@synthi.net>
2013-04-15 11:19:48 +0200

    Fix space issue in internal_base.

--- src/Qumulus/Uml/Core/Abstractions/internal_base.h
+++ src/Qumulus/Uml/Core/Abstractions/internal_base.h
@@ -8,7 +8,7 @@
 #define QUMULUS_UML_CORE_ABSTRACTIONS_INTERNAL_BASE_H_
 
 #define QUML_BEGIN_NAMESPACE_UCA \
-    namespace Qumulus { namespace Uml { namespace Core { \ 
+    namespace Qumulus { namespace Uml { namespace Core { \
         namespace Abstractions { 
 #define QUML_END_NAMESPACE_UCA }}}}
 
