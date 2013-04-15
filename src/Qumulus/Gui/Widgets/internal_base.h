8d3fff6089311338f716f10cf8d6af3f6b1c0b2c
Randy Thiemann <uselinuxnow@gmail.com>
2013-04-15 10:39:21 +0200

    Add internal_base for Gui::Widgets

---
+++ src/Qumulus/Gui/Widgets/internal_base.h
@@ -0,0 +1,21 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#ifndef QUMULUS_GUI_WIDGETS_INTERNAL_BASE_H_
+#define QUMULUS_GUI_WIDGETS_INTERNAL_BASE_H_
+
+#define QUML_BEGIN_NAMESPACE_GW \
+    namespace Qumulus { namespace Gui { namespace Widgets {
+
+#define QUML_END_NAMESPACE_GW }}}
+
+QUML_BEGIN_NAMESPACE_GW
+QUML_END_NAMESPACE_GW
+
+namespace QuGW = Qumulus::Gui::Widgets;
+
+#endif /* QUMULUS_GUI_WIDGETS_INTERNAL_BASE_H_ */
+
