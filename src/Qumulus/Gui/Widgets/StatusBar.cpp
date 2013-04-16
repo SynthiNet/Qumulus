19b6f337be53a33f66db68e1025c3b98fe58ec42
Randy Thiemann <uselinuxnow@gmail.com>
2013-04-16 11:08:03 +0200

    Add StatusBar.

---
+++ src/Qumulus/Gui/Widgets/StatusBar.cpp
@@ -0,0 +1,53 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#include "StatusBar.h"
+#include <Gui/Widgets/MainWindow.h>
+
+QUML_BEGIN_NAMESPACE_GW
+
+StatusBar::StatusBar(QWidget* parent) : QStatusBar(parent) {
+#ifdef Q_OS_MAC
+    setSizeGripEnabled(false);
+    setStyleType(StyleType::Active);
+#endif
+}
+
+void StatusBar::setWindow(MainWindow* w) {
+    mWindow = w;
+}
+
+MainWindow* StatusBar::window() {
+    return mWindow;
+}
+
+#ifdef Q_OS_MAC
+void StatusBar::setStyleType(StyleType s) {
+    switch(s) {
+    case StyleType::Active:
+        setStyleSheet(
+            "border-top: 1px solid #828282;"
+            "background-color: qlineargradient("
+                    "x1: 0, y1: 0, x2: 0, y2: 1,"
+                    "stop: 0 rgb(212, 212, 212),"
+                    "stop: 1 rgb(176, 176, 176));"
+        );
+        break;
+    case StyleType::Inactive:
+        setStyleSheet(
+            "border-top: 1px solid #B2B2B2;"
+            "background-color: qlineargradient("
+                    "x1: 0, y1: 0, x2: 0, y2: 1,"
+                    "stop: 0 rgb(237, 237, 237),"
+                    "stop: 1 rgb(224, 224, 224));"
+        );
+        break;
+    }
+}
+#endif
+
+QUML_END_NAMESPACE_GW
+
---
+++ src/Qumulus/Gui/Widgets/StatusBar.h
@@ -0,0 +1,37 @@
+/*
+ * Qumulus UML editor
+ * Author: Randy Thiemann
+ *
+ */
+
+#ifndef GUI_WIDGETS_STATUSBAR_H_
+#define GUI_WIDGETS_STATUSBAR_H_
+
+#include "internal_base.h"
+#include <QtWidgets/QStatusBar>
+#include <Gui/Widgets/StyleType.h>
+
+QUML_BEGIN_NAMESPACE_GW
+
+class MainWindow;
+
+class StatusBar : public QStatusBar {
+    Q_OBJECT
+
+public:
+    StatusBar(QWidget* parent = 0);
+    void setWindow(MainWindow* w);
+    MainWindow* window();
+
+#ifdef Q_OS_MAC
+    void setStyleType(StyleType s);
+#endif
+
+private:
+    MainWindow* mWindow;
+};
+
+QUML_END_NAMESPACE_GW
+
+#endif /* GUI_WIDGETS_STATUSBAR_H_ */
+
