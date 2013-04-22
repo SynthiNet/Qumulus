b9f0f39510cbdc5d399a4a50c050f47d336874dd
Randy Thiemann <uselinuxnow@gmail.com>
2013-04-22 13:58:07 +0200

    Fix ZoomSlider.

--- src/Qumulus/Gui/Widgets/ZoomSlider.cpp
+++ src/Qumulus/Gui/Widgets/ZoomSlider.cpp
@@ -8,7 +8,15 @@
 
 QUML_BEGIN_NAMESPACE_GW
 
-ZoomSlider::ZoomSlider(QWidget* parent) : QSlider(parent) {}
+ZoomSlider::ZoomSlider(QWidget* parent) : QSlider(parent) {
+    setStyleSheet("QSlider {"
+            "border: 5px solid transparent; background: transparent;}");
+    setOrientation(Qt::Horizontal);
+    setMaximumWidth(150);
+    setMinimum(25);
+    setMaximum(400);
+    setSliderPosition(100);
+}
 
 QUML_END_NAMESPACE_GW
 
