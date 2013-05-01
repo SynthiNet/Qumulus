15efdd8b647c5a673f417c84dc61ec9080760d24
Randy Thiemann <uselinuxnow@gmail.com>
2013-05-01 15:28:43 +0200

    Adding some scrollbars into the mix, because we can.

--- src/Qumulus/Gui/Widgets/EditorView.cpp
+++ src/Qumulus/Gui/Widgets/EditorView.cpp
@@ -12,6 +12,9 @@ EditorView::EditorView(QWidget* parent) : QGraphicsView(parent),
     mScene(new QGraphicsScene(this)) {
     mScene->addRect(0, 0, 20, 20);
     setScene(mScene);
+    setSceneRect(-20000.0, -20000.0, 40000.0, 40000.0);
+    setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
+    setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOn);
 }
 
 void EditorView::zoom(double value) {
