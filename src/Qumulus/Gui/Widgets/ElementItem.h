44ae192e0430592ab87b087de944571b61df8b31
Randy Thiemann <uselinuxnow@gmail.com>
2013-05-01 13:33:23 +0200

    Add slot mechanism to ElementItem.

--- src/Qumulus/Gui/Widgets/ElementItem.h
+++ src/Qumulus/Gui/Widgets/ElementItem.h
@@ -11,25 +11,30 @@
 #include <QtCore/QString>
 #include <QtGui/QIcon>
 #include <QtGui/QKeySequence>
+#include <functional>
 
 QUML_BEGIN_NAMESPACE_GW
 
 class ElementItem {
 public:
     ElementItem() {}
-    ElementItem(QString text, QIcon icon, QKeySequence shortcut) :
-        mText(text), mIcon(icon), mShortcut(shortcut) {}
+    ElementItem(QString text, QIcon icon, QKeySequence shortcut, 
+            std::function<void()> slot) :
+        mText(text), mIcon(icon), mShortcut(shortcut), mSlot(slot) {}
     void setText(QString s) { mText = s; }
     QString text() const { return mText; }
     void setIcon(QIcon i) { mIcon = i; }
     QIcon icon() const { return mIcon; }
     void setShortcut(QKeySequence k) { mShortcut = k; }
     QKeySequence shortcut() const { return mShortcut; }
+    void setSlot(std::function<void()> slot) { mSlot = slot; }
+    std::function<void()> slot() const { return mSlot; }
 
 private:
     QString mText;
     QIcon mIcon;
     QKeySequence mShortcut;
+    std::function<void()> mSlot;
 };
 
 QUML_END_NAMESPACE_GW
