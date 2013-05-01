4722c2b8618cf1986fb0dbaba47c401068a295ff
Frank Erens <frank@synthi.net>
2013-05-01 12:22:09 +0200

    More pointer stuff.

--- src/Qumulus/Lib/Core/Ptr.h
+++ src/Qumulus/Lib/Core/Ptr.h
@@ -17,5 +17,18 @@ using uptr = std::unique_ptr<T>;
 template<class T>
 using sptr = std::shared_ptr<T>;
 
+template<class T>
+using wptr = std::weak_ptr<T>;
+
+template<class T, class... A>
+uptr<T> make_unique(A&&... a) {
+    return uptr<T>(new T(std::forward(a)...));
+}
+
+template<class T, class... A>
+sptr<T> make_shared(A&&... a) {
+    return sptr<T>(new T(std::forward(a)...));
+}
+
 #endif /* QUMULUS_LIB_CORE_PTR_H_ */
 
