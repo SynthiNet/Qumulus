9d20950cb812d5d44f13f0534dfc0ac575409568
Frank Erens <frank@synthi.net>
2013-05-01 12:03:50 +0200

    Add LeafElement.

---
+++ src/Qumulus/Uml/Diagram/LeafElement.h
@@ -0,0 +1,19 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_DIAGRAM_LEAFELEMENT_H_
+#define UML_DIAGRAM_LEAFELEMENT_H_
+
+#include "internal_base.h"
+
+#include "DiagramElement.h"
+
+class LeafElement : public DiagramElement {
+
+};
+
+#endif /* UML_DIAGRAM_LEAFELEMENT_H_ */
+
