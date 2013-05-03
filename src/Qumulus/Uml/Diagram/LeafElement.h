3cfb382aa90a4a472156feee41a8dbaf8d535421
Frank Erens <frank@synthi.net>
2013-05-03 17:24:23 +0200

    Fix namespace on LeafElement.

--- src/Qumulus/Uml/Diagram/LeafElement.h
+++ src/Qumulus/Uml/Diagram/LeafElement.h
@@ -11,9 +11,13 @@
 
 #include "DiagramElement.h"
 
+QUML_BEGIN_NAMESPACE_UD
+
 class LeafElement : public DiagramElement {
 
 };
+
+QUML_END_NAMESPACE_UD
 
 #endif /* UML_DIAGRAM_LEAFELEMENT_H_ */
 
