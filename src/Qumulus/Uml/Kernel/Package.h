e5d6a4dc38e80eecec78c19d755fbbed8a9dbab2
Frank Erens <frank@synthi.net>
2013-05-05 15:32:49 +0200

    Update Package.

--- src/Qumulus/Uml/Kernel/Package.h
+++ src/Qumulus/Uml/Kernel/Package.h
@@ -16,20 +16,43 @@
 
 QUML_BEGIN_NAMESPACE_UK
 
+class Type;
+
 class Package : public Namespace, public PackageableElement {
 public:
-
-    const QSet<Package*>& nestedPackages() const {
-        return mNestedPackages;
-    }
+    Package();
+    Package(QString name, Package* p = 0);
 
     bool mustBeOwned() const override {
         return false;
     }
 
+    const QSet<PackageableElement*> packagedElements() const {
+        return mPackagedElements;
+    }
+
+    const QSet<Package*>& nestedPackages() const {
+        return mNestedPackages;
+    }
+
+    void addNestedPackage(Package* p);
+    void removeNestedPackage(Package* p);
+
+    const QSet<Type*> ownedTypes() const {
+        return mOwnedTypes;
+    }
+
+    void addOwnedType(Type* t);
+    void removeOwnedType(Type* t);
+
     QUML_CLONABLE(Package)
 private:
+    void addPackagedElement(PackageableElement* e);
+    void removePackagedElement(PackageableElement* e);
+private:
+    QSet<PackageableElement*> mPackagedElements;
     QSet<Package*> mNestedPackages;
+    QSet<Type*> mOwnedTypes;
 };
 
 QUML_END_NAMESPACE_UK
