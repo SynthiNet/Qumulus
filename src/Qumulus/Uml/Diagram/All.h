057ebe3c9ba52840c96d4954742a6a4a8a163163
Frank Erens <frank@synthi.net>
2013-05-06 10:29:13 +0200

    Re-add All.h, internal_base.h

---
+++ src/Qumulus/Uml/Diagram/All.h
@@ -0,0 +1,12 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_DIAGRAM_ALL_H_
+#define UML_DIAGRAM_ALL_H_
+
+
+#endif /* UML_DIAGRAM_ALL_H_ */
+
---
+++ src/Qumulus/Uml/Diagram/internal_base.h
@@ -0,0 +1,23 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_DIAGRAM_INTERNAL_BASE_H_
+#define UML_DIAGRAM_INTERNAL_BASE_H_
+
+
+// Code here
+#define QUML_BEGIN_NAMESPACE_UD \
+    namespace Qumulus { namespace Uml { namespace Diagram {
+
+#define QUML_END_NAMESPACE_UD }}}
+
+QUML_BEGIN_NAMESPACE_LC
+QUML_END_NAMESPACE_LC
+
+namespace QuUD = Qumulus::Uml::Diagram;
+
+#endif /* UML_DIAGRAM_INTERNAL_BASE_H_ */
+
