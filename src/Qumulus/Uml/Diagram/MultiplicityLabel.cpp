766c6ac1b2e96d20bdcd0069fb4d08cba767532f
Frank Erens <frank@synthi.net>
2013-05-08 13:54:10 +0200

    Add MultiplicityLabel.

---
+++ src/Qumulus/Uml/Diagram/MultiplicityLabel.cpp
@@ -0,0 +1,24 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#include "MultiplicityLabel.h"
+
+QUML_BEGIN_NAMESPACE_UD
+
+
+KeywordLabel::KeywordLabel(QuUK::Element* e, DiagramElement* p) :
+        Label(e, p) {
+
+}
+
+KeywordLabel::KeywordLabel(const QString& t, QuUK::Element* e, 
+        DiagramElement* p) :
+        Label(t, e, p) {
+
+}
+
+QUML_END_NAMESPACE_UD
+
