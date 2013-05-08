70d5eaa1540a7039d444066029d6062d580c18f7
Frank Erens <frank@synthi.net>
2013-05-08 11:33:45 +0200

    Add AssociationShapeKind.

---
+++ src/Qumulus/Uml/Diagram/AssociationShapeKind.h
@@ -0,0 +1,22 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_DIAGRAM_ASSOCIATIONSHAPEKIND_H_
+#define UML_DIAGRAM_ASSOCIATIONSHAPEKIND_H_
+
+#include "internal_base.h"
+
+QUML_BEGIN_NAMESPACE_UD
+
+enum class AssociationShapeKind {
+    Diamond,
+    Triangle
+};
+
+QUML_END_NAMESPACE_UD
+
+#endif /* UML_DIAGRAM_ASSOCIATIONSHAPEKIND_H_ */
+
