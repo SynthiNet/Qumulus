cc85b29a27075102db59c60cf6aaa5775c80227d
Frank Erens <frank@synthi.net>
2013-05-10 09:15:15 +0200

    Initialize random generator.

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,12 @@
 #include <Gui/Core/QumulusApplication.h>
 #include <Gui/Widgets/MainWindow.h>
 
+#include <cstdlib>
+#include <ctime>
+
 int main(int argc, char** argv) {
+    std::srand(std::time(nullptr));
+
     QuGC::QumulusApplication app(argc, argv);
 
     QuGW::MainWindow w;
