c00d5a2ae2aac980619e893573639f80a39cac6e
Frank Erens <frank@synthi.net>
2013-05-11 10:54:33 +0200

    Set font size correctly on Mac and non-Mac.

--- src/Qumulus/Uml/Diagram/Diagram.cpp
+++ src/Qumulus/Uml/Diagram/Diagram.cpp
@@ -9,13 +9,20 @@
 
 QUML_BEGIN_NAMESPACE_UD
 
+constexpr static float kFontSize =
+#ifdef Q_OS_MAC
+        12.0;
+#else
+        10.0;
+#endif
+
 Diagram::Diagram(QString name, double resolution) :
         mName(name),
         mResolution(resolution) {
     auto s = new Style;
     setLocalStyle(s);
     s->setFontName("sans-serif");
-    s->setFontSize(12.0);
+    s->setFontSize(kFontSize);
 }
 
 Diagram::Diagram(const Diagram& d) :
