0b1b59f1910c4507c7932bca9c80d72c58cc3206
Randy Thiemann <uselinuxnow@gmail.com>
2013-05-13 16:01:13 +0200

    Fix transparent background for PackageShape and modify inheritance tree.

--- src/Qumulus/Uml/Diagram/PackageShape.h
+++ src/Qumulus/Uml/Diagram/PackageShape.h
@@ -9,13 +9,13 @@
 
 #include "internal_base.h"
 
-#include "Shape.h"
+#include "SelectableShape.h"
 
 #include <QtWidgets/QGraphicsRectItem>
 
 QUML_BEGIN_NAMESPACE_UD
 
-class PackageShape : public Shape {
+class PackageShape : public SelectableShape {
 public:
     PackageShape(QuUK::Element* e = 0, DiagramElement* p = 0);
     PackageShape(const PackageShape&);
