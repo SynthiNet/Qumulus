1e5a48d906d87ca9eb0cd65a0a61ea52da5e145a
Frank Erens <frank@synthi.net>
2013-05-15 11:16:49 +0200

    Add Generalization.

---
+++ src/Qumulus/Uml/Kernel/DirectedRelationship.h
@@ -0,0 +1,40 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_KERNEL_DIRECTEDRELATIONSHIP_H_
+#define UML_KERNEL_DIRECTEDRELATIONSHIP_H_
+
+#include "internal_base.h"
+
+#include "Element.h"
+
+QUML_BEGIN_NAMESPACE_UK
+
+class DirectedRelationship : public Element {
+public:
+    DirectedRelationship() = default;
+    DirectedRelationship(Element* source, Element* target) : 
+            mSource(source),
+            mTarget(target) {
+
+    }
+
+    Element* source() const { return mSource; }
+    void setSource(Element* s) { mSource = s; }
+
+    Element* target() const { return mTarget; }
+    void setTarget(Element* s) { mTarget = s; }
+
+    QUML_CLONABLE_ABSTRACT(DirectedRelationship);
+private:
+    Element* mSource = nullptr;
+    Element* mTarget = nullptr;
+};
+
+QUML_END_NAMESPACE_UK
+
+#endif /* UML_KERNEL_DIRECTEDRELATIONSHIP_H_ */
+
---
+++ src/Qumulus/Uml/Kernel/Generalization.h
@@ -0,0 +1,36 @@
+/*
+ * Qumulus UML editor
+ * Author: Frank Erens
+ *
+ */
+
+#ifndef UML_KERNEL_GENERALIZATION_H_
+#define UML_KERNEL_GENERALIZATION_H_
+
+#include "internal_base.h"
+
+#include "DirectedRelationship.h"
+
+QUML_BEGIN_NAMESPACE_UK
+class Classifier;
+
+class Generalization : public DirectedRelationship {
+public:
+    Generalization();
+    Generalization(Classifier* general, Classifier* specific);
+    
+    Classifier* general() const;
+    void setGeneral(Classifier*);
+
+    Classifier* specific() const;
+    void setSpecific(Classifier*);
+
+    void updateDiagramElement(QuUD::Diagram*, QSizeF) override {}
+
+    QUML_CLONABLE(Generalization);
+};
+
+QUML_END_NAMESPACE_UK
+
+#endif /* UML_KERNEL_GENERALIZATION_H_ */
+
