8ee4c4e99ff135dc82a8accf7c9b7d3ccb891bba
Frank Erens <frank@synthi.net>
2013-05-23 07:07:37 +0200

    Multiplicities no longer optional.

--- src/Qumulus/Uml/Kernel/MultiplicityElement.cpp
+++ src/Qumulus/Uml/Kernel/MultiplicityElement.cpp
@@ -11,15 +11,12 @@
 
 QUML_BEGIN_NAMESPACE_UK
 
-MultiplicityElement::MultiplicityElement() {
+MultiplicityElement::MultiplicityElement() :
+        mLower(1),
+        mUpper(1) {
 
 }
 
-MultiplicityElement::MultiplicityElement(QuLC::Unlimited u) :
-        mLower(QuLC::unltd(u) ? nullptr : u),
-        mUpper(u) {
-
-}
 MultiplicityElement::MultiplicityElement(unsigned l, QuLC::Unlimited u) : 
         mLower(l),
         mUpper(u) {
@@ -28,37 +25,32 @@ MultiplicityElement::MultiplicityElement(unsigned l, QuLC::Unlimited u) :
 
 
 bool MultiplicityElement::isMultivalued() const { 
-    return mUpper && *mUpper > 1; 
+    return mUpper > 1; 
 }
 
 bool MultiplicityElement::includesCardinality(unsigned i) const { 
-    return mLower && mUpper && *mLower <= i && i <= *mUpper;
+    return mLower <= i && i <= mUpper;
 }
 
 bool MultiplicityElement::includedMultiplicity(MultiplicityElement* m) const {
-    return mLower && mUpper && m->mLower && m->mUpper &&
-            *mLower <= *m->mLower && *m->mUpper <= *mUpper;
+    return mLower <= m->mLower && m->mUpper <= mUpper;
 }
 
 QString MultiplicityElement::multiplicityString() const {
     QString str = "";
-    if(mLower || mUpper) {
-        unsigned lower = mLower ? *(mLower) : 0;
-        QuLC::Unlimited upper = mUpper ? *(mUpper) : "*";
 
-        if(lower == 0 || QuLC::unltd(upper)) {
-            str += "[*]";
-        } else if(QuLC::unltd(upper) || lower != upper) {
-            str += "[";
-            str += lower;
-            str += "..";
-            str += upper;
-            str += "]";
-        } else if(lower != 1) {
-            str += "[";
-            str += lower;
-            str += "]";
-        }
+    if(mLower == 0 || QuLC::unltd(mUpper)) {
+        str += "[*]";
+    } else if(QuLC::unltd(mUpper) || mLower != mUpper) {
+        str += "[";
+        str += mLower;
+        str += "..";
+        str += mUpper;
+        str += "]";
+    } else if(mLower != 1) {
+        str += "[";
+        str += mLower;
+        str += "]";
     }
 
     return str;
