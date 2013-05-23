6c6e6976c4db16daf392baef716dfb4846cfc4db
Frank Erens <frank@synthi.net>
2013-05-23 14:31:26 +0200

    ClassShape now properly renders abstract and static elements.

--- src/Qumulus/Gui/Diagram/ClassShape.cpp
+++ src/Qumulus/Gui/Diagram/ClassShape.cpp
@@ -8,6 +8,11 @@
 
 #include "Compartment.h"
 
+#include <Uml/Kernel/Property.h>
+#include <Uml/Kernel/Operation.h>
+
+#include <QtGui/QPainter>
+
 QUML_BEGIN_NAMESPACE_GD
 
 ClassShape::ClassShape(QuUK::Element* e, DiagramElement* p) : 
@@ -32,17 +37,64 @@ QuUK::Class* ClassShape::modelElement() const {
 }
 
 void ClassShape::updateSizeConstraints() {
-    setMinimumSize({std::max(190, classNameWidth()) + 10, 100});
+    float fheight = QFontMetrics(sharedStyle()->font()).height() * 1.2; 
+    float aheight = std::max(30.0f, 
+            modelElement()->attributes().size() * fheight) + 10;
+    float oheight = std::max(30.0f, 
+            modelElement()->operations().size() * fheight) + 10;
+
+    setMinimumSize({std::max(190, classNameWidth()) + 10, 
+            20 + aheight + oheight});
     mHeadCompartment->setMinimumHeight(20);
     mHeadCompartment->setMaximumHeight(20);
-    mAttributeCompartment->setMinimumHeight(40);
-    mOperationCompartment->setMinimumHeight(40);
+    mAttributeCompartment->setMinimumHeight(aheight);
+    mOperationCompartment->setMinimumHeight(oheight);
 }
 
 void ClassShape::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, 
         QWidget* widget) {
+    updateSizeConstraints();
+    resize(size());
+
     CompartmentableShape::paint(painter, option, widget);
 
+    QFont nameFont = sharedStyle()->font();
+    nameFont.setBold(true);
+    if(modelElement()->abstract()) 
+        nameFont.setItalic(true);
+
+    painter->setFont(nameFont);
+    painter->drawText(0, 0, width(), 20,
+            Qt::AlignHCenter | Qt::AlignVCenter,
+            modelElement()->name());
+
+    QFont featureFont = sharedStyle()->font();
+    QFont staticFeatureFont = featureFont;
+    staticFeatureFont.setUnderline(true);
+    QFontMetrics met(featureFont);
+
+    float th = 20;
+    float delta = met.height() * 1.2;
+
+    for(auto& a : modelElement()->attributes()) {
+        painter->setFont(a->isStatic() ? staticFeatureFont : featureFont);
+        painter->drawText(2, th, width() - 2, delta,
+                Qt::AlignLeft | Qt::AlignVCenter,
+                met.elidedText(a->toString(), 
+                        Qt::ElideRight, width() - 4));
+        th += delta;
+    }
+
+    th = 20 + mAttributeCompartment->height();
+
+    for(auto& o : modelElement()->operations()) {
+        painter->setFont(o->isStatic() ? staticFeatureFont : featureFont);
+        painter->drawText(2, th, width() - 2, delta,
+                Qt::AlignLeft | Qt::AlignVCenter,
+                met.elidedText(o->toString(), 
+                        Qt::ElideRight, width() - 4));
+        th += delta;
+    }
 }
 
 int ClassShape::classNameWidth() const {
