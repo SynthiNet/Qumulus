aa57cbb40ba5adee8203f55b4ea9ce6cdb87d426
Randy Thiemann <uselinuxnow@gmail.com>
2013-05-27 16:16:52 +0200

    Allow placing Elements in the view via the shortcuts.

--- src/Qumulus/Gui/Widgets/EditorView.h
+++ src/Qumulus/Gui/Widgets/EditorView.h
@@ -10,7 +10,9 @@
 #include "internal_base.h"
 #include <QtWidgets/QGraphicsView>
 #include <QtGui/QMouseEvent>
+#include <QtCore/QPointF>
 #include <Gui/Diagram/Diagram.h>
+#include <Gui/Widgets/MainWindow.h>
 
 class QPushButton;
 class QGraphicsProxyWidget;
@@ -19,11 +21,16 @@ QUML_BEGIN_NAMESPACE_GW
 
 class Popover;
 
+struct MouseState {
+    QPointF startPos;
+    CursorState startCursor;
+};
+
 class EditorView : public QGraphicsView {
     Q_OBJECT
 
 public:
-    EditorView(QWidget* parent = 0);
+    EditorView(MainWindow* parent = 0);
     ~EditorView() noexcept;
     const QuGD::Diagram* diagram() const { return mDiagram; }
 
@@ -35,12 +42,16 @@ public slots:
 
 protected:
     void mousePressEvent(QMouseEvent* event) override;
+    void mouseDoubleClickEvent(QMouseEvent* event) override;
+    void mouseReleaseEvent(QMouseEvent* event) override;
     void mouseMoveEvent(QMouseEvent* event) override;
     void keyPressEvent(QKeyEvent* event) override;
     void keyReleaseEvent(QKeyEvent* event) override;
+    void wheelEvent(QWheelEvent* event) override;
 
 private:
     QGraphicsScene* mScene;
+    QGraphicsRectItem* mSelectionRect;
     Popover* mPopover;
     QuGD::Diagram* mDiagram;
     bool mCursorOverride = false;
@@ -52,6 +63,12 @@ private:
     QGraphicsProxyWidget* mOperationButtonItem;
     QGraphicsProxyWidget* mLiteralButtonItem;
 
+    MouseState mMouseState;
+    QPointF mMousePosition;
+    MainWindow* mMainWindow;
+    bool mIsDragging = false;
+    double mCurrentZoom = 1;
+    bool mScrollable = true;
 };
 
 QUML_END_NAMESPACE_GW
